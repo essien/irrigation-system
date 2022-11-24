140b882a83ec77ebde964e9df0ab9a68e49b081d
Emmanuel Essiennta <colourfulemmanuel@gmail.com>
2022-11-24 21:22:14 +0100

    Basic setup

---
+++ src/main/java/com/github/essien/banquemisr/irrigation/IrrigationApplication.java
@@ -0,0 +1,12 @@
+package com.github.essien.banquemisr.irrigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrrigationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/essien/banquemisr/irrigation/IrrigationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.essien.banquemisr.irrigation;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IrrigationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
